1eded4e9366f12a540df70001acd6027f1a4c632
AjeetBind <bindajeet209@gmail.com>
2026-02-22 14:52:31 +0530

    update login code

--- src/test/java/pageObjects/LoginPage.java
+++ src/test/java/pageObjects/LoginPage.java
@@ -1,43 +1,61 @@
 package pageObjects;
 
+import java.time.Duration;
+
+import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
 public class LoginPage extends BasePage {
-	WebDriver driver;
 
-	public LoginPage(WebDriver driver) {
-		super(driver);
-	}
+    WebDriver driver;
 
-	@FindBy(id = "txtUsername")
-	private WebElement txtuserName;
-	@FindBy(id = "txtPassword")
-	private WebElement txtuserpassword;
-	@FindBy(id="btnLogin")
-	private WebElement btnLogin;
-	@FindBy(xpath = "//img[@src='/assets/Images/ERPApp.svg']")
-	private WebElement ERPApps;
-	@FindBy(xpath = "//div[@class='LinkSrvc ng-scope']/div[contains(text(),'I Card')]")
-	private WebElement Icard;
+    public LoginPage(WebDriver driver) {
+        super(driver);
+        this.driver = driver;
+    }
 
+    @FindBy(id = "txtUsername")
+    private WebElement txtuserName;
 
-	public void setUserName(String username){
-		txtuserName.sendKeys(username);
-	}
-	public void setPasswordName(String userpassword) {
+    @FindBy(id = "txtPassword")
+    private WebElement txtuserpassword;
 
-		txtuserpassword.sendKeys(userpassword);
-	}
+    @FindBy(id = "btnLogin")
+    private WebElement btnLogin;
 
-	public void clickbtnLogin() {
-		btnLogin.click();
-	}
-	public void clickERPApps() {
-		ERPApps.click();
-	}
-	public void clickIcard() {
-		Icard.click();
-	}
+    @FindBy(xpath = "//img[@src='/assets/Images/ERPApp.svg']")
+    private WebElement ERPApps;
+
+    @FindBy(xpath = "//div[@class='LinkSrvc ng-scope']/div[contains(text(),'I Card')]")
+    private WebElement Icard;
+
+    public void setUserName(String username) {
+        txtuserName.sendKeys(username);
+    }
+
+    public void setPasswordName(String userpassword) {
+        txtuserpassword.sendKeys(userpassword);
+    }
+
+    public void clickbtnLogin() {
+        btnLogin.click();
+    }
+
+    public void clickERPApps() {
+        ERPApps.click();
+    }
+
+    public void clickIcard() {
+
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(15));
+
+        wait.until(ExpectedConditions.elementToBeClickable(Icard));
+
+        JavascriptExecutor js = (JavascriptExecutor) driver;
+        js.executeScript("arguments[0].click();", Icard);
+    }
 }
