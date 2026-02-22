82827efba1d330c4b0b0bb4bdafb1d365cbd832a
AjeetBind <bindajeet209@gmail.com>
2026-02-22 14:26:52 +0530

    Added headless chrome options

--- src/test/java/testCases/Baseclass.java
+++ src/test/java/testCases/Baseclass.java
@@ -5,6 +5,7 @@ import java.time.Duration;
 import org.apache.commons.lang3.RandomStringUtils;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
@@ -15,11 +16,18 @@ public class Baseclass {
 	WebDriver driver;
 	@BeforeClass
 	public void setup() {
-		driver=  new ChromeDriver();
-		driver.manage().deleteAllCookies();
-		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
-		driver.get("https://appuat.franciscanecare.net/Login/Identifier?SchCode=DEMOIN");
-		driver.manage().window().maximize();
+
+	    ChromeOptions options = new ChromeOptions();
+	    options.addArguments("--headless"); // run without UI
+	    options.addArguments("--no-sandbox");
+	    options.addArguments("--disable-dev-shm-usage");
+	    options.addArguments("--start-maximized");
+
+	    driver = new ChromeDriver(options);
+
+	    driver.manage().deleteAllCookies();
+	    driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+	    driver.get("https://appuat.franciscanecare.net/Login/Identifier?SchCode=DEMOIN");
 	}
 
 
