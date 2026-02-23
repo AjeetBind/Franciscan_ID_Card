c8f063c0f3f3eba490ced1b67fa10b34b0475927
AjeetBind <bindajeet209@gmail.com>
2026-02-23 18:47:18 +0530

    update login page

--- src/test/java/testCases/TC001_LoginpageTest.java
+++ src/test/java/testCases/TC001_LoginpageTest.java
@@ -1,39 +1,24 @@
 package testCases;
 
-import org.testng.Assert;
-import org.testng.annotations.BeforeClass;
+
 import org.testng.annotations.Test;
 
 import pageObjects.LoginPage;
 
 public class TC001_LoginpageTest extends Baseclass {
+	
+	@Test
+	public void testLoginWithValidCredentials() {
 
-    LoginPage lp;
-
-    @BeforeClass
-    public void loginOnce() {
-
-        lp = new LoginPage(driver);
-
-        lp.setUserName("sf001");
-        lp.setPasswordName("Francis@1988");
-        lp.clickbtnLogin();
-         System.out.println(driver.getCurrentUrl());
-        // Optional: validate login success
-        Assert.assertTrue(driver.getCurrentUrl().contains("dashboard"),
-                "Login failed -Invalid username or password!");
-    }
-
-    @Test
-    public void verifyClickOnERPApps() {
-        lp.clickERPApps();
-        Assert.assertTrue(true);  // Replace with real validation
-    }
-
-    @Test
-    public void verifyClickOnIcard() {
-        lp.clickERPApps();
-        lp.clickIcard();
-        Assert.assertTrue(true);  // Replace with real validation
-    }
+		LoginPage lp=new LoginPage(driver);
+		lp.setUserName("sf001");
+		lp.setPasswordName("Francis1988");
+		lp.clickbtnLogin();
+		lp.clickERPApps();
+		lp.clickIcard();
+	}
 }
+
+
+
+
